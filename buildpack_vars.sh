c04565ea27e701f141f71d572a6c5e9d21675c2c
Micah Lee <micah.lee@cyberark.com>
2019-05-07 10:21:24 -0400

    make the buildpack name configurable

--- buildpack_vars.sh
+++ buildpack_vars.sh
@@ -3,6 +3,6 @@
 version=$(cat VERSION)
 
 SRC_DIR=$(cd "$(dirname $0)/."  && pwd)
-NAME=$(basename "$SRC_DIR" | sed s/-/_/g)
+NAME=${BUILDPACK_NAME:-$(basename "$SRC_DIR" | sed s/-/_/g)}
 TGT_DIR=${SRC_DIR}
 ZIP_FILE="$TGT_DIR/$NAME-$version.zip"
