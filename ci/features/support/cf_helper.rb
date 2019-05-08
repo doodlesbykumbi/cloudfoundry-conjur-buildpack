a7c8ba2b7db2d1c59603643a58511b04151f9e09
Micah Lee <micah.lee@cyberark.com>
2019-05-08 13:25:18 -0400

    remove unused code from `cf_helper.rb`

--- ci/features/support/cf_helper.rb
+++ ci/features/support/cf_helper.rb
@@ -28,16 +28,7 @@ module CfHelper
 
     res.body.strip
   end
-
-  def org_guid(org_name)
-    ShellSession.execute(%(cf org --guid "#{org_name}")).output.chomp
-  end
-
-  def space_guid(org_name, space_name)
-    cf_target(org_name, space_name)
-    ShellSession.execute(%(cf space --guid "#{space_name}")).output.chomp
-  end
-
+  
   def login_to_pcf
     api_endpoint = ENV['CF_API_ENDPOINT']
 
@@ -58,13 +49,6 @@ module CfHelper
     {
       username: username,
       password: password
-    }
-  end
-
-  def admin_user
-    @admin_user ||= {
-      username: 'admin',
-      password: CF_ADMIN_PASSWORD
     }
   end
 
@@ -96,30 +80,6 @@ module CfHelper
     name
   end
 
-  def ci_app_route
-    route = ShellSession.execute(<<~SHELL
-      cf app hello-world | \
-      awk -F ':' -v key="routes" '$1==key {print $2}'
-    SHELL
-                                ).output.strip!
-      
-    "https://#{route}/"
-  end
-
-  def ci_app_content
-    uri = URI(ci_app_route)
-    req = Net::HTTP::Get.new(uri.path)
-
-    res = Net::HTTP.start(
-            uri.host, uri.port, 
-            :use_ssl => uri.scheme == 'https', 
-            :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
-      https.request(req)
-    end
-
-    res.body.strip!
-  end
-
   def cf_api(api)
     ShellSession.execute(%(cf api "#{api}" --skip-ssl-validation))
   end
@@ -134,9 +94,5 @@ module CfHelper
     else
       ShellSession.execute(%(cf target -o "#{org}"))
     end
-  end
-
-  def cf_service_instance_id
-    @cf_service_instance_id ||= ShellSession.execute(%(cf service --guid conjur)).output.chomp
   end
 end
